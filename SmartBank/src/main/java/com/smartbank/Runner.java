66b0e1907a9f5a5e0360ba8cfa212cc21f91561b
ayounsni <Ayoubsnini@gmail.com>
2024-10-09 17:20:37 +0100

    testBackEnd

--- SmartBank/src/main/java/com/smartbank/Runner.java
+++ SmartBank/src/main/java/com/smartbank/Runner.java
@@ -1,11 +1,24 @@
 package com.smartbank;
 
 import com.smartbank.config.EntityManagerFactorySingleton;
+import com.smartbank.models.entities.Status;
+import com.smartbank.repository.implementations.StatusRepository;
+import com.smartbank.repository.interfaces.IStatusRepository;
 import jakarta.persistence.EntityManagerFactory;
 
 public class Runner {
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = EntityManagerFactorySingleton.entityManagerFactory();
+//        EntityManagerFactory entityManagerFactory = EntityManagerFactorySingleton.entityManagerFactory();
+
+        StatusRepository statusRepository = new StatusRepository();
+        Status status = new Status();
+        status.setNom("Rejeter");
+        statusRepository.persist(status);
+
+
+
+
+
 
     }
 }
